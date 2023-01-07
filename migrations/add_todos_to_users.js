5f16f540614c0a0515b8f0245c2610300d14492f
Narenreddyb007 <113661393+Narenreddyb007@users.noreply.github.com>
2023-01-08 00:32:26 +0530

    Update add_todos_to_users.js

--- migrations/add_todos_to_users.js
+++ migrations/add_todos_to_users.js
@@ -3,9 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Todos','userId',{
-      type: Sequelize.DataTypes.INTEGER 
-    })
+   
     await queryInterface.addConstraint('Todos',{
       fields:['userId'],
       type:'foreign key',
